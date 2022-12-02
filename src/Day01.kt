464976490724798d0031ce687a4414bbdbf6072d
Armand Mégrot <armand.megrot@gmail.com>
2022-12-02 22:50:48 +0100

    Finish day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,35 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxCalories = 0
+        var sum = 0
+        input.forEach {
+            if (it == "") {
+                if (sum > maxCalories) maxCalories = sum
+                sum = 0
+            } else {
+                sum += it.toInt()
+            }
+        }
+        return maxCalories
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var top3 = arrayOf(0, 0, 0)
+        var sum = 0
+        input.forEach {
+            if (it == "") {
+                top3 = (arrayOf(sum) + top3).sortedArray().reversedArray().take(3).toTypedArray()
+                sum = 0
+            } else {
+                sum += it.toInt()
+            }
+        }
+        return top3.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
