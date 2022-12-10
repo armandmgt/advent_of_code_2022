47b677d8e5f7edc39186d8a31762e8720b4f67dd
Armand Mégrot <armand.megrot@gmail.com>
2022-12-10 23:08:18 +0100

    Day09 part2

--- src/Day09.kt
+++ src/Day09.kt
@@ -2,7 +2,7 @@ import kotlin.math.abs
 import kotlin.math.sign
 
 fun main() {
-    data class State(val headPos: Pair<Int, Int>, val tailPos: Pair<Int, Int>)
+    data class State(val headPos: Pair<Int, Int>, val tailNodes: List<Pair<Int, Int>>)
 
     fun follow(head: Int, tail: Int, headOther: Int, tailOther: Int): Pair<Int, Int> {
         // If tail is 2 squares away from head, move in direction of head
@@ -26,26 +26,44 @@ fun main() {
                 Direction.WEST -> Pair(s.headPos.first - 1, s.headPos.second)
                 Direction.EAST -> Pair(s.headPos.first + 1, s.headPos.second)
             }
-            var newTailPosX = s.tailPos.first
-            var newTailPosY = s.tailPos.second
+            var prevNode = newHeadPos
+            val newTailNodes = s.tailNodes.map { tailNode ->
+                var newTailPosX = tailNode.first
+                var newTailPosY = tailNode.second
 
-            follow(newHeadPos.first, s.tailPos.first, newHeadPos.second, s.tailPos.second).also {
-                newTailPosX += it.first
-                newTailPosY += it.second
-            }
-            follow(newHeadPos.second, s.tailPos.second, newHeadPos.first, s.tailPos.first).also {
-                newTailPosY += it.first
-                newTailPosX += it.second
-            }
+                follow(prevNode.first, tailNode.first, prevNode.second, tailNode.second).also {
+                    newTailPosX += it.first
+                    newTailPosY += it.second
+                }
+                follow(prevNode.second, tailNode.second, prevNode.first, tailNode.first).also {
+                    newTailPosY += it.first
+                    newTailPosX += it.second
+                }
 
-            val newTailPos = Pair(newTailPosX, newTailPosY)
-            visited.add(newTailPos)
-            State(newHeadPos, newTailPos)
+                Pair(newTailPosX, newTailPosY).also { prevNode = it }
+            }
+            visited.add(newTailNodes.last())
+            State(newHeadPos, newTailNodes)
         }
     }
 
-    fun part1(input: List<String>): Int {
-        val state = State(Pair(0, 0), Pair(0, 0))
+//    fun printState(it: State) {
+//        for (y in -16 until 5) {
+//            for (x in -11 until 15) {
+//                print(
+//                    when {
+//                        it.headPos.first == x && it.headPos.second == y -> 'H'
+//                        it.tailNodes.contains(Pair(x, y)) -> '0' + it.tailNodes.indexOf(Pair(x, y))
+//                        else -> '.'
+//                    }
+//                )
+//            }
+//            print('\n')
+//        }
+//        println()
+//    }
+
+    fun parseMotions(input: List<String>, state: State): Int {
         val visited = mutableSetOf<Pair<Int, Int>>()
         input.fold(state) { s, motion ->
             val dir = Direction.from(motion[0])
@@ -55,8 +73,14 @@ fun main() {
         return visited.size
     }
 
+    fun part1(input: List<String>): Int {
+        val state = State(Pair(0, 0), listOf(Pair(0, 0)))
+        return parseMotions(input, state)
+    }
+
     fun part2(input: List<String>): Int {
-        return 0
+        val state = State(Pair(0, 0), List(9) { Pair(0, 0) })
+        return parseMotions(input, state)
     }
 
     // test if implementation meets criteria from the description, like:
@@ -67,6 +91,6 @@ fun main() {
     println(part1(input))
 
     val testInput2 = readInput("resources/Day09_test2")
-    check(part2(testInput) == 36)
+    check(part2(testInput2) == 36)
     println(part2(input))
 }
